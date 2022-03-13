32be99f84b8d966abfc770ef7c6245ce5ff4bc93
JeDeveloppe <jedeveloppe.contact@gmail.com>
2022-03-13 18:08:20 +0100

    start dashboard user

--- src/Controller/Site/UserController.php
+++ src/Controller/Site/UserController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller\Site;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user/dashboard", name="user_dashboard")
+     * @Route("/espace-client/dashboard", name="user_dashboard")
      */
-    public function index(): Response
+    public function index(Security $security): Response
     {
-        return $this->render('site/user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        $user = $security->getUser();
+
+        if(!$user){
+            //message flash
+            $this->addFlash('danger', 'Vous n\'êtes pas identifié(e) !');
+            return $this->redirectToRoute('app_login');
+
+        }else{
+
+            return $this->render('site/espace_client/dashboard.html.twig', [
+                'user' => $user,
+            ]);
+
+        }
     }
 }
